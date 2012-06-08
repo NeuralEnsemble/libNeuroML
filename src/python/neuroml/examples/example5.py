01a412336b412df3cae6550a2ae04001c0236995
Mike <mike@jarvis.chu.cam.ac.uk>
2012-06-08 13:32:30 +0100

    added an example

--- src/python/neuroml/examples/example5.py
+++ src/python/neuroml/examples/example5.py
@@ -1,4 +1,8 @@
-#translating a morphology:
+"""
+This example shows how a morphology can be translated
+such that the translated node moves to the new origin
+and all other nodes move along with it
+"""
 import neuroml.morphology as ml
 
 a=ml.Node([1,2,3,10])
