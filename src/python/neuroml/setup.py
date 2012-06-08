04a75aad950935c2593a547ac62f9e52d999b6ff
Mike <mike@jarvis.chu.cam.ac.uk>
2012-06-08 02:20:10 +0100

    added setup.py

---
+++ src/python/neuroml/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='neuroml',
+      version='0.0.1.1',
+      description='Python API for libNeuroML',
+      author='Mike Vella',
+      author_email='vellamike@gmail.com',
+      url='https://github.com/NeuralEnsemble/libNeuroML',
+      packages=['neuroml'],
+     )
