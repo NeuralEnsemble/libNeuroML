8f7f09f45d70598f0eb64f639f6907df8c2386bd
Mike <mike@jarvis.chu.cam.ac.uk>
2012-06-08 13:30:48 +0100

    Improved the example documentation

--- src/python/neuroml/examples/example3.py
+++ src/python/neuroml/examples/example3.py
@@ -1,29 +1,47 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 Michael Vella
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 
+#  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#-------------------------------------------------------------------------------
+
 """
-Simple node connectivity
+This example shows how segments are created and manipulated and
+how a morphology is loaded
 """
 
-import neuroml.morphology as ml
+import neuroml.morphology as morphology
+import neuroml.loaders as loaders
 
-a=ml.Node()
-b=ml.Node()
-c=ml.Node()
-a.connect(b)
-c.connect(b)
-chain1=a.morphology
-#now create and connect a new node:
-d=ml.Node()
-d.connect(c)
-chain2=d.morphology
+#example of creating a segment:
+iseg=morphology.Segment(length=20.0)
+axonsec1=morphology.Segment(length=1000)
+node1=morphology.Segment(length=20)
+axonsec2=morphology.Segment(length=500)
+axonsec3=morphology.Segment()
 
-#now let's make yet another chain:
-f=ml.Node()
-g=ml.Node()
-h=ml.Node()
-f.connect(g)
-g.connect(h)
+#connect them all together:
+#standard is child.connect(parent)
+axonsec1.connect(iseg)
+node1.connect(axonsec1)
+axonsec2.connect(node1)
+axonsec3.connect(axonsec2)
 
-chain3=g.morphology
-chain3[0].connect(chain1[1])
-morph1=a.morphology
+#print some information from the morphology (nodecollection) backend: -
+#potentially it would make more sense to offer a segmentcollection view of this too
+artificial_morphology=iseg.morphology
+print 'Artificial morphology vertices:'
+print artificial_morphology.vertices
+print 'Artificial morphology connectivity:'
+print artificial_morphology.connectivity
 
-
+#Load a morphology from an SWC file and print some info about it
+morph1=loaders.SWCLoader.load_swc_single('/home/mike/tmp2mod.swc')
+print 'Loaded connectivity'
+print morph1.connectivity
