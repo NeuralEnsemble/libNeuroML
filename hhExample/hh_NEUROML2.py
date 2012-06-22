e8fa166d6578ab572264cc0c7ccd02c826b5a429
Padraig Gleeson <p.gleeson@gmail.com>
2012-06-22 13:09:54 +0100

    Working simple hh_NEUROML2.py

--- hhExample/hh_NEUROML2.py
+++ hhExample/hh_NEUROML2.py
@@ -1,7 +1,10 @@
 
 #############   Some imports
 
+# package from ../ideas/padraig/generatedFromV2Schema/
 from neuroml2 import NeuroMLReader
+from neuroml2.v2 import Cell
+from neuroml2.v2.NeuroMLDocument import NeuroMLDocument, validate_nml2
 
 
 
@@ -9,16 +12,26 @@ from neuroml2 import NeuroMLReader
 
 reader = NeuroMLReader()
 
-filename = "../../../testFiles/CA1.nml"
-print "Reading in NeuroML 2 file: "+ filename
-nml2Doc = reader.read_neuroml(filename)
-print "Read in cells in v2 file with Id: "+nml2Doc.id
+filename = "../testFiles/CA1.nml"
+nml2_doc = reader.read_neuroml(filename)
+original_cell = nml2_doc.cell[0]
+ca1_morph = original_cell.morphology
+print "Read in morphology with %i segments" % len(ca1_morph.segment)
 
 
 #############  Load the ion channels
 
+nml2_doc = reader.read_neuroml("../testFiles/NaF.nml")
+naf_chan = nml2_doc.ionChannel[0]
+nml2_doc = reader.read_neuroml("../testFiles/Kdr.nml")
+kdr_chan = nml2_doc.ionChannel[0]
+nml2_doc = reader.read_neuroml("../testFiles/Leak.nml")
+leak_chan = nml2_doc.ionChannel[0]
 
 #############  Define cell types (templates)
+
+ca1_cell = Cell(id="CA1")
+ca1_cell.morphology = ca1_morph
 
 
 #############  Define populations
@@ -36,3 +49,13 @@ print "Read in cells in v2 file with Id: "+nml2Doc.id
 #############  Run simulation
 
 # NOTE: not running simulation, just saving NeuroML to file..
+new_nml_doc = NeuroMLDocument(id="MyNet")
+new_nml_doc.add_cell(ca1_cell)
+new_nml_doc.add_ionChannel(naf_chan)
+new_nml_doc.add_ionChannel(kdr_chan)
+new_nml_doc.add_ionChannel(leak_chan)
+
+new_nml_file = "MyNet.xml"
+new_nml_doc.write_neuroml(new_nml_file)
+
+validate_nml2(new_nml_file)
