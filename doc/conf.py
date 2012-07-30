d8c579f750dba22ff1cf26c9236ed8505f0e8e98
Mike Vella <mv333@cam.ac.uk>
2012-07-30 17:34:17 +0100

    configuring sys.path

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 sys.path.insert(0, os.path.abspath('../neuroml'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
