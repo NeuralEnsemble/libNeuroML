877318ae7324cafefcc0b36c614ba6545e412e13
Mike Vella <vellamike@gmail.com>
2012-09-20 01:35:42 +0100

    added some inline doc

--- neuroml/utils/xml_case_convert.py
+++ neuroml/utils/xml_case_convert.py
@@ -1,3 +1,7 @@
+"""
+Working on util to convert naming conventions from neuroml to something more Python-friendly
+"""
+
 def convert(name):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
