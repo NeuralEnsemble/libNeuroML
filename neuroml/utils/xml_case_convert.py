bd8c9247daf60dce5decf65e4d1503a0595770f0
Mike Vella <vellamike@gmail.com>
2012-09-24 16:11:50 +0100

    adding reparsing code

--- neuroml/utils/xml_case_convert.py
+++ neuroml/utils/xml_case_convert.py
@@ -5,9 +5,11 @@ Working on util to convert naming conventions from neuroml to something more Pyt
 import re
 
 def to_lowercase_with_underscores(string):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 def remove_curlies(string):
-    return re.sub("{.*}","",s)
+    return re.sub("{.*}","",string)
+
+#def capitalise_first(string):
 
