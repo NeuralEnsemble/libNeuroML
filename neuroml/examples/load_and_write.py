1eb6770466a38d43b19969fcb1f002f4e3c1eb9f
Mike Vella <vellamike@gmail.com>
2013-03-06 11:52:56 +0000

    fix file typo

--- neuroml/examples/load_and_write.py
+++ neuroml/examples/load_and_write.py
@@ -1,7 +1,7 @@
 import neuroml
 from neuroml import loaders
 
-doc = loaders.NeuroMLLoader.load('Purk2M9s.nml')
+doc = loaders.NeuroMLLoader.load('./test_files/Purk2M9s.nml')
 
 f = open('./tmp/test_load_and_write.xml','w')
 doc.export(f,0)
