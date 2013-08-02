32ac4f1eb805432e8fcecc6c8e44d698187edc8b
Mike Vella <vellamike@gmail.com>
2013-08-02 18:42:32 +0100

    initial HDF5 serialization complete

--- neuroml/test/test_writers.py
+++ neuroml/test/test_writers.py
@@ -6,13 +6,15 @@ Unit tests for writers
 import neuroml
 from neuroml import writers
 import os
+import numpy as np
+from neuroml import arraymorph as am
 
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
 
-class TestNeuroMLLoader(unittest.TestCase):
+class TestNeuroMLWriter(unittest.TestCase):
 
     def test_write_nonsense(self):
         """
@@ -22,3 +24,31 @@ class TestNeuroMLLoader(unittest.TestCase):
         a = 6 #not NeuroML-writeable
         writer_method = neuroml.writers.NeuroMLWriter.write
         self.assertRaises(AttributeError, writer_method, a, "tmpfile")
+
+class TestArrayMorphWriter(unittest.TestCase):
+    
+    def setUp(self):
+        num_segments = int(1e6)
+        num_vertices = num_segments + 1
+
+        x = np.linspace(0,10,num_vertices)
+        y = np.zeros(num_vertices)
+        z = np.zeros(num_vertices)
+        d = np.linspace(1,0.01,num_vertices)
+
+        vertices = np.array([x,y,z,d]).T
+        
+        connectivity = range(-1,num_segments)
+
+        big_arraymorph = am.ArrayMorphology(vertices = vertices,
+                                            connectivity = connectivity)
+
+        self.big_arraymorph = big_arraymorph
+
+    def test_write_big_arraymorph(self):
+        writer_method = neuroml.writers.ArrayMorphWriter.write
+        writer_method(self.big_arraymorph,'dev/null')        
+        try:
+            writer_method(self.big_arraymorph,'dev/null')
+        except:
+            self.fail("Exception raised!")
--- neuroml/writers.py
+++ neuroml/writers.py
@@ -1,5 +1,7 @@
 import neuroml
 import numpy as np
+import tables
+
 
 class NeuroMLWriter(object):
     @classmethod
@@ -21,16 +23,48 @@ class NeuroMLWriter(object):
         nmldoc.export(f,0,name_="neuroml",
                       namespacedef_=namespacedef) #name_ param to ensure root element named correctly - generateDS limitation
 
+class HDF5ArrayMorph(tables.IsDescription):
+    vertices = tables.Float32Col(dflt=1, pos = 1)
+    connectivity =  tables.Float32Col(dflt=1, pos = 1)
+    physical_mask = tables.Float32Col(dflt=1, pos = 1)
+
+
 class ArrayMorphWriter(object):
+    """
+    For now just testing a simple method which can write a morphology, not a NeuroMLDocument.
+    """
+
     @classmethod
-    def write(cls,arraymorph,file_path):
+    def __write_single_morphology(cls,array_morph,fileh):
+        vertices = array_morph.vertices
+        connectivity = array_morph.connectivity
+        physical_mask = array_morph.physical_mask
 
-        import h5py
+        # Get the HDF5 root group
+        root = fileh.root
+        
+        # Create the groups:
+        # can use morphology name in future?
+        morphology_name = "morphology_1"
+        morphology_group = fileh.createGroup(root, morphology_name)
 
-        f = h5py.File(file_path,'w')
-        identifier = arraymorph.id
+        # Now, create an array in root group
+        vertices_array = fileh.createArray("/"+morphology_name, "vertices", vertices)
+        connectivity_array = fileh.createArray("/"+morphology_name, "connectivity", connectivity)
+        physical_mask_array = fileh.createArray("/"+morphology_name, "physical_mask", physical_mask)
 
-        f['vertices_'+str(identifier)] = arraymorph.vertices
-        f['connectivity_'+str(identifier)] = arraymorph.connectivity
+    @classmethod
+    def write(cls,array_morph,file_path):
 
-        f.close()
+        assert isinstance(array_morph,neuroml.Morphology)
+
+        # Open a file in "w"rite mode
+        fileh = tables.openFile("test_file.h5", mode = "w")
+        
+        #Now instead we should go through a document/cell/morphology
+        #hierarchy - this kind of tree traversal should be done recursively
+
+        cls.__write_single_morphology(array_morph, fileh)
+
+        # Finally, close the file (this also will flush all the remaining buffers!)
+        fileh.close()
