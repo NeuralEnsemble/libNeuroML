73a9693cea6f51eb326a65a3b458a185513ae570
Mike Vella <vellamike@gmail.com>
2013-08-06 13:55:16 +0100

    playing around with logo size

--- doc/conf.py
+++ doc/conf.py
@@ -113,7 +113,7 @@ html_theme = 'nature'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'neuroml_small_logo.gif'
+html_logo = 'neuroml_medium_logo.gif'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
