7ee9c10eb5306f4bdbb8e1c6dc1310f72f755bf9
Mike Vella <vellamike@gmail.com>
2013-08-17 04:24:12 +0100

    removing stress test, its not the right place for it

--- neuroml/test/test_writers.py
+++ neuroml/test/test_writers.py
@@ -19,7 +19,7 @@ except ImportError:
 class TestJSONWriter(unittest.TestCase):
     
     def setUp(self):
-        num_segments = int(3.3e5) #~1M segments. Stress test.
+        num_segments = int(1e4) #Per cell
         num_vertices = num_segments + 1
 
         x = np.linspace(0,10,num_vertices)
