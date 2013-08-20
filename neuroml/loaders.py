b2ec247929afaef85c47d99503b0467f49a28b0b
Mike Vella <vellamike@gmail.com>
2013-08-20 11:48:55 +0100

    jsonpickle version not properly installing Revert "no longer bundling jsonpickle - maintainer has uploaded latest version to pypi"
    This reverts commit ea86892cacf42c5696b1ebbaf03f5b4116b36dc7.

--- neuroml/loaders.py
+++ neuroml/loaders.py
@@ -2,7 +2,7 @@ import numpy as np
 from nml.nml import parse as nmlparse
 from neuroml import arraymorph
 import neuroml
-from jsonpickle import decode as json_decode
+from .jsonpickle import decode as json_decode
 import neuroml
 
 class NeuroMLLoader(object):
