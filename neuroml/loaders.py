22f3906ad487542f6b07a58a4f505995f0866c01
Sho Iizuka <sho.i518@gmail.com>
2013-09-12 14:55:44 +0900

    Backend -> arraymorph.ArrayMorphology

--- neuroml/loaders.py
+++ neuroml/loaders.py
@@ -70,7 +70,7 @@ class SWCLoader(object):
                 connection_indices[i]=-1
 
         #This needs to become an "Optimized Morphology" of some kind
-        return Backend(vertices=vertices, 
+        return arraymorph.ArrayMorphology(vertices=vertices, 
                        connectivity=connection_indices, 
                        name=name )
 
