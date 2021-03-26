580d54e9e8c2b5fc516b3549bf84e3b949e7bdde
Padraig Gleeson <p.gleeson@gmail.com>
2021-03-26 09:50:07 +0000

    Bump to v0.2.56 for dev

--- neuroml/__init__.py
+++ neuroml/__init__.py
@@ -1,6 +1,6 @@
 from .nml.nml import *  # allows importation of all neuroml classes
 
-__version__ = '0.2.55'
+__version__ = '0.2.56'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
 
 
