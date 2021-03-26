1d2367ce1c49f86846d6e40d0ed4f36e8ea4d629
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-03-26 11:40:18 +0000

    fix(nml.py): update generateds config script

--- neuroml/nml/generateds_config.py
+++ neuroml/nml/generateds_config.py
@@ -2,21 +2,27 @@
 #The class names are essentially correct, the instance names need converting
 #also attributes need fixing
 
-import lxml
 from lxml import objectify
 import re
 from config import variables
 import csv
 import sys
+import io
+import process_includes
+import keyword
+
 
 sys.setrecursionlimit(10000)
+
 
 def remove_curlies(string):
     return re.sub("{.*}","",string)
 
+
 def to_lowercase_with_underscores(string):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
 
 def to_camelback(string):
     string_list = list(string)
@@ -29,6 +35,7 @@ def to_camelback(string):
     string = ''.join(string_list)
 
     return str(string)
+
 
 def traverse_doc(queue,rename):
     """Recursive function to traverse the nodes of a tree in
@@ -47,20 +54,22 @@ def traverse_doc(queue,rename):
     else:
         return None
 
-def pluralize(noun):                            
-    if re.search('[sxz]$', noun):             
-        return re.sub('$', 'es', noun)        
+
+def pluralize(noun):
+    if re.search('[sxz]$', noun):
+        return re.sub('$', 'es', noun)
     elif re.search('[^aeioudgkprt]h$', noun):
-        return re.sub('$', 'es', noun)       
-    elif re.search('[^aeiou]y$', noun):      
-        return re.sub('y$', 'ies', noun)     
-    else:                                    
+        return re.sub('$', 'es', noun)
+    elif re.search('[^aeiou]y$', noun):
+        return re.sub('y$', 'ies', noun)
+    else:
         return noun + 's'
+
 
 def _node_to_python(node):
 
     pluralize_flag = 'maxOccurs' in node.attrib
-    
+
     for attribute in node.attrib:
         nml_attribute = node.attrib.pop(attribute)
         if nml_attribute[0].islower():
@@ -69,16 +78,18 @@ def _node_to_python(node):
                 renamed_attribute = pluralize(renamed_attribute)
             NameTable[nml_attribute] = renamed_attribute
 
+
+# Print out that this is running
+# Otherwise generateds silently proceeds, and our nml.py file is all wrong
+print("generateds_config.py is being processed")
+
 filename = variables['schema_name']
 
-import StringIO
-import process_includes
 
-outfile = StringIO.StringIO()
+outfile = io.StringIO()
 infile = open(filename, 'r')
 
-process_includes.process_include_files(infile, outfile,
-        inpath=filename)
+process_includes.process_include_files(infile, outfile, inpath=filename)
 infile.close()
 outfile.seek(0)
 doc = objectify.parse(outfile)
@@ -89,27 +100,24 @@ NameTable = {}
 
 traverse_doc(queue,_node_to_python)
 
-#filtering routine, need to get a better way to extract these, asked on Stack Overflow
-import keyword
+# filtering routine, need to get a better way to extract these, asked on Stack Overflow
 disallowed_keywords = keyword.kwlist
-for keyword in disallowed_keywords:
+for kw in disallowed_keywords:
     try:
-        NameTable.pop(keyword)
-    except:
+        NameTable.pop(kw)
+    except Exception:
         pass
 
-NameTable['morphology'] = 'morphology' #overriding change to
-#"morphologies" because it only applies outside of a cell - not a very
-#elegant solution
+# overriding change to "morphologies" because it only applies outside of a cell
+# - not a very elegant solution
+NameTable['morphology'] = 'morphology'
 NameTable['gateHHtauInf'] = 'gate_hh_tau_infs'
 NameTable['ionChannelHH'] = 'ion_channel_hh'
-NameTable['gateHHrates']  = 'gate_hh_rates'
+NameTable['gateHHrates'] = 'gate_hh_rates'
 NameTable['gateHHtauInf'] = 'gate_hh_tau_infs'
 
 NameTable['ionChannel'] = 'ion_channel'
 NameTable['ionChannelHH'] = 'ion_channel_hhs'
-
-
 
 NameTable['basePyNNCell'] = 'basePyNNCell'
 NameTable['basePyNNIaFCell'] = 'basePyNNIaFCell'
@@ -136,17 +144,18 @@ NameTable['extracellularProperties'] = 'extracellular_properties'
 NameTable['intracellularProperties'] = 'intracellular_properties'
 NameTable['biophysicalProperties'] = 'biophysical_properties'
 
-print("NameTable is as follows:")
-print(NameTable)
+nametable_fn = "name_table.csv"
+print("Saving NameTable to csv file: {}".format(nametable_fn))
+with open(nametable_fn, 'w', newline='') as nametable_fh:
+    writer = csv.writer(nametable_fh)
+    for key, value in list(NameTable.items()):
+        writer.writerow([key, value])
 
-print("Saving NameTable to csv file")
-writer = csv.writer(open('name_table.csv', 'wb'))
-for key, value in NameTable.items():
-   writer.writerow([key, value])
-
-print ("Saving name changes table to csv file")
-changes_writer = csv.writer(open('changed_names.csv','wb'))
-for key in NameTable:
-    value = NameTable[key]
-    if key != value:
-        changes_writer.writerow([key,value])
+changes_fn = "changed_names.csv"
+print("Saving name changes table to csv file: {}".format(changes_fn))
+with open(changes_fn, 'w', newline='') as changes_fh:
+    changes_writer = csv.writer(changes_fh)
+    for key in NameTable:
+        value = NameTable[key]
+        if key != value:
+            changes_writer.writerow([key,value])
