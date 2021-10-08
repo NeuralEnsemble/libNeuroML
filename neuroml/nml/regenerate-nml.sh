4a7cfe9904709fecdc6b30f1404384820dd2f4da
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-10-08 16:12:57 +0100

    refactor: improve nml.py generator script

--- neuroml/nml/regenerate-nml.sh
+++ neuroml/nml/regenerate-nml.sh
@@ -10,21 +10,75 @@ echo "Note(2): Must be run in neuroml/nml/"
 NEUROML_VERSION=$(grep -E 'current_neuroml_version.*' ../__init__.py | cut -d '=' -f 2 | tr -d '"' | tr -d ' ')
 SCHEMA_FILE=NeuroML_${NEUROML_VERSION}.xsd
 
-if command -v generateDS > /dev/null 2>&1
+regenerate () {
+    if command -v generateDS > /dev/null 2>&1
+    then
+        echo "Rebuilding nml.py from ${SCHEMA_FILE} (Version: ${NEUROML_VERSION})"
+        echo "generateds version:"
+        generateDS --version
+
+        rm -f nml.py
+        PYTHONPATH="$PYTHONPATH:." generateDS -o nml.py --use-getter-setter=none --user-methods=helper_methods.py $SCHEMA_FILE
+    else
+        echo "GenerateDS not installed"
+        echo "Run: pip install generateds"
+        exit 1
+    fi
+}
+
+reformat () {
+    if command -v generateDS > /dev/null 2>&1
+    then
+        echo "Formatting new nml.py with black"
+        black nml.py
+        exit 0
+    else
+        echo "black is not installed"
+        echo "Run: pip install black"
+        exit 1
+    fi
+}
+
+usage () {
+    echo "$0: Regenerate and reformat nml.py from the XSD schema file"
+    echo
+    echo "Usage: $0 [-arfh]"
+    echo
+    echo "-r: regenerate"
+    echo "-f: reformat"
+    echo "-a: regenerate and reformat"
+    echo "-h: print this help text and exit"
+}
+
+if [ $# -lt 1 ]
 then
-    echo "Rebuilding nml.py from ${SCHEMA_FILE} (Version: ${NEUROML_VERSION})"
-    echo "generateds version:"
-    generateDS --version
-
-    rm -f nml.py
-    PYTHONPATH="$PYTHONPATH:." generateDS -o nml.py --use-getter-setter=none --user-methods=helper_methods.py $SCHEMA_FILE
-
-    echo "Formatting new nml.py with black"
-    black nml.py
-else
-    echo "GenerateDS not installed"
-    echo "Run: pip install generateds"
+    usage
     exit 1
 fi
 
-exit 0
+# parse options
+while getopts "arfh" OPTION
+do
+    case $OPTION in
+        r)
+            regenerate
+            exit 0
+            ;;
+        f)
+            reformat
+            exit 0
+            ;;
+        a)
+            regenerate && reformat
+            exit 0
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
