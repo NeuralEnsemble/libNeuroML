914cb2cd808c2db854074fdfc53442bfb226db70
Padraig Gleeson <p.gleeson@gmail.com>
2021-11-24 11:18:30 +0000

    To v0.3.1

--- neuroml/__init__.py
+++ neuroml/__init__.py
@@ -1,6 +1,6 @@
 from .nml.nml import *  # allows importation of all neuroml classes
 
-__version__ = "0.2.58"
+__version__ = "0.3.1"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
 
 
