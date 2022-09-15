2fdbb90e6fd12499782e5c6c5a25531d62500c42
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-09-15 11:29:09 +0100

    feat: move version out into separate file

--- neuroml/__init__.py
+++ neuroml/__init__.py
@@ -1,7 +1,2 @@
 from .nml.nml import *  # allows importation of all neuroml classes
-
-__version__ = "0.4.1"
-__version_info__ = tuple(int(i) for i in __version__.split("."))
-
-
-current_neuroml_version = "v2.3"
+from .__version__ import *
---
+++ neuroml/__version__.py
@@ -0,0 +1,15 @@
+"""
+libNeuroML version information
+
+File: neuroml/__version__.py
+
+Copyright 2022 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+
+__version__: str = "0.4.1"
+__version_info__: tuple = tuple(int(i) for i in __version__.split("."))
+
+
+current_neuroml_version: str = "v2.3"
