1950b04402b6a7d712c135382229068fbc3de640
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-09-20 18:42:25 +0100

    chore(nml.py): regenerate

--- neuroml/nml/nml.py
+++ neuroml/nml/nml.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Wed Sep  7 14:15:49 2022 by generateDS.py version 2.40.13.
-# Python 3.10.6 (main, Aug  2 2022, 00:00:00) [GCC 12.1.1 20220507 (Red Hat 12.1.1-1)]
+# Generated Tue Sep 20 18:32:03 2022 by generateDS.py version 2.40.13.
+# Python 3.10.7 (main, Sep  7 2022, 00:00:00) [GCC 12.2.1 20220819 (Red Hat 12.2.1-1)]
 #
 # Command line options:
 #   ('-o', 'nml.py')
@@ -15,7 +15,7 @@
 #   NeuroML_v2.3.xsd
 #
 # Command line:
-#   /home/asinha/.local/share/virtualenvs/pyneuroml-dev/bin/generateDS -o "nml.py" --use-getter-setter="none" --user-methods="helper_methods.py" --export="write validate" NeuroML_v2.3.xsd
+#   /home/asinha/.local/share/virtualenvs/neuroml-dev/bin/generateDS -o "nml.py" --use-getter-setter="none" --user-methods="helper_methods.py" --export="write validate" NeuroML_v2.3.xsd
 #
 # Current working directory (os.getcwd()):
 #   nml
@@ -46136,6 +46136,8 @@ class Cell(BaseCell):
             obj_.original_tagname_ = "biophysicalProperties"
         super(Cell, self)._buildChildren(child_, node, nodeName_, True)
 
+    from ..neuro_lex_ids import neuro_lex_ids
+
     # Get segment object by its id
     def get_segment(self, segment_id):
         # type: (str) -> Segment
@@ -46532,6 +46534,351 @@ class Cell(BaseCell):
         print("* Segments: " + str(len(self.morphology.segments)))
         print("* SegmentGroups: " + str(len(self.morphology.segment_groups)))
         print("*******************************************************")
+
+    def add_segment(
+        self,
+        prox,
+        dist,
+        name=None,
+        parent=None,
+        fraction_along=1.0,
+        group=None,
+        use_convention=True,
+    ):
+        """Add a segment to the cell.
+
+        Suggested convention: use `axon_`, `soma_`, `dend_` prefixes for axon,
+        soma, and dendrite segments respectivey. This will allow this function to
+        add the correct neurolex IDs to the group.
+
+        If `use_convention` is true, the created segment is also added to the
+        default segment groups that were created by the `create_cell` function:
+        `all`, `dendrite_group`, `soma_group`, `axon_group`. Note that while it is
+        not necessary to use the convention, it is necessary to be consistent.
+
+        :param prox: proximal segment information
+        :type prox: list with 4 float entries: [x, y, z, diameter]
+        :param dist: dist segment information
+        :type dist: list with 4 float entries: [x, y, z, diameter]
+        :param name: name of segment
+        :type name: str
+        :param parent: parent segment
+        :type parent: SegmentParent
+        :param fraction_along: where the new segment is connected to the parent (0: distal point, 1: proximal point)
+        :type fraction_along: float
+        :param group: segment group to add the segment to
+        :type group: SegmentGroup
+        :param use_convention: whether helper segment groups should be created using the default convention
+        :type use_convention: bool
+        :returns: the created segment
+
+        """
+        try:
+            if prox:
+                p = self.component_factory(
+                    "Point3DWithDiam", x=prox[0], y=prox[1], z=prox[2], diameter=prox[3]
+                )
+            else:
+                p = None
+        except IndexError as e:
+            print("{}: prox must be a list of 4 elements".format(e))
+        try:
+            d = self.component_factory(
+                "Point3DWithDiam", x=dist[0], y=dist[1], z=dist[2], diameter=dist[3]
+            )
+        except IndexError as e:
+            print("{}: dist must be a list of 4 elements".format(e))
+
+        segid = len(self.morphology.segments)
+
+        if segid > 0 and parent is None:
+            raise Exception(
+                "There are currently more than one segments in the cell, but one is being added without specifying a parent segment"
+            )
+
+        sp = (
+            self.component_factory(
+                "SegmentParent", segments=parent.id, fraction_along=fraction_along
+            )
+            if parent
+            else None
+        )
+        segment = self.component_factory(
+            "Segment", id=segid, proximal=p, distal=d, parent=sp
+        )
+
+        if name:
+            segment.name = name
+
+        if group:
+            seg_group = None
+            seg_group_default = None
+            neuro_lex_id = None
+
+            # cell.get_segment_group throws an exception of the segment group
+            # does not exist
+            try:
+                seg_group = self.get_segment_group(group)
+            except Exception as e:
+                print("Warning: {}".format(e))
+
+            if "axon_" in group:
+                neuro_lex_id = self.neuro_lex_ids[
+                    "axon"
+                ]  # See http://amigo.geneontology.org/amigo/term/GO:0030424
+                if use_convention:
+                    seg_group_default = self.get_segment_group("axon_group")
+            if "soma_" in group:
+                neuro_lex_id = self.neuro_lex_ids["soma"]
+                if use_convention:
+                    seg_group_default = self.get_segment_group("soma_group")
+            if "dend_" in group:
+                neuro_lex_id = self.neuro_lex_ids["dend"]
+                if use_convention:
+                    seg_group_default = self.get_segment_group("dendrite_group")
+
+            if seg_group is None:
+                seg_group = self.component_factory(
+                    "SegmentGroup", id=group, neuro_lex_id=neuro_lex_id
+                )
+                self.morphology.add(seg_group, validate=False)
+
+            seg_group.add(self.component_factory("Member", segments=segment.id))
+            # Ideally, these higher level segment groups should just include other
+            # segment groups using Include, which would result in smaller NML
+            # files. However, because these default segment groups are defined
+            # first, they are printed in the NML file before the new segments and their
+            # groups. The jnml validator does not like this.
+            # TODO: clarify if the order of definition is important, or if the jnml
+            # validator needs to be updated to manage this use case.
+            if use_convention and seg_group_default:
+                seg_group_default.add(
+                    self.component_factory("Member", segments=segment.id)
+                )
+
+        if use_convention:
+            seg_group_all = self.get_segment_group("all")
+            seg_group_all.add(self.component_factory("Member", segments=segment.id))
+
+        self.morphology.add(segment)
+        return segment
+
+    def set_init_memb_potential(self, v, group="all"):
+        """Set the initial membrane potential of the cell.
+
+        :param v: value to set for membrane potential with units
+        :type v: str
+        :param group: id of segment group to modify
+        :type group: str
+        """
+        self.biophysical_properties.membrane_properties.add(
+            "InitMembPotential", value=v, segment_groups=group
+        )
+
+    def set_resistivity(self, resistivity, group="all") -> None:
+        """Set the resistivity of the cell
+
+        :type resistivity: str
+        :param group: segment group to modify
+        :type group: str
+        """
+        self.biophysical_properties.intracellular_properties.add(
+            "Resistivity", value=resistivity, segment_groups=group
+        )
+
+    def set_specific_capacitance(self, spec_cap, group="all"):
+        """Set the specific capacitance for the cell.
+
+        :param spec_cap: value of specific capacitance with units
+        :type spec_cap: str
+        :param group: segment group to modify
+        :type group: str
+        """
+        self.biophysical_properties.membrane_properties.add(
+            "SpecificCapacitance", value=spec_cap, segment_groups=group
+        )
+
+    def add_intracellular_property(self, property_name, **kwargs):
+        """Generic function to add an intracellular property to the cell.
+
+        For a full list of membrane properties, see:
+        https://docs.neuroml.org/Userdocs/Schemas/Cells.html?#intracellularproperties
+
+        :param property_name: name of intracellular property to add
+        :type property_name: str
+        :param kwargs: named arguments for intracellular property to be added
+        :type kwargs: Any
+        :returns: None
+
+        """
+        self.biophysical_properties.intracellular_properties.add(
+            property_name, **kwargs
+        )
+
+    def add_membrane_property(self, property_name, **kwargs):
+        """Generic function to add a membrane property to the cell.
+
+        For a full list of membrane properties, see:
+        https://docs.neuroml.org/Userdocs/Schemas/Cells.html?#membraneproperties
+
+        Please also see specific functions in this module, which are designed to be
+        easier to use than this generic function.
+
+        :param property_name: name of membrane to add
+        :type property_name: str
+        :param kwargs: named arguments for membrane property to be added
+        :type kwargs: Any
+        :returns: None
+
+        """
+        self.biophysical_properties.membrane_properties.add(property_name, **kwargs)
+
+    def add_channel_density_v(
+        self, channel_density_type, nml_cell_doc, ion_chan_def_file="", **kwargs
+    ):
+        """Generic function to add channel density components to a Cell.
+
+        :param channel_density_type: type of channel density to add.
+            See https://docs.neuroml.org/Userdocs/Schemas/Cells.html for the
+            complete list.
+        :type channel_density_type: str
+        :param nml_cell_doc: cell NeuroML document to which channel density is to be added
+        :type nml_cell_doc: NeuroMLDocument
+        :param ion_chan_def_file: path to NeuroML2 file defining the ion channel, if empty, it assumes the channel is defined in the same file
+        :type ion_chan_def_file: str
+        :param kwargs: named arguments for required channel density type
+        :type kwargs: Any
+        :returns: None
+        """
+
+        self.add_membrane_property(channel_density_type, **kwargs)
+
+        if len(ion_chan_def_file) > 0:
+            if (
+                self.component_factory("IncludeType", ion_chan_def_file)
+                not in nml_cell_doc.includes
+            ):
+                nml_cell_doc.add("IncludeType", ion_chan_def_file)
+
+    def add_channel_density(
+        self,
+        nml_cell_doc,
+        cd_id,
+        ion_channel,
+        cond_density,
+        erev="0.0 mV",
+        group="all",
+        ion="non_specific",
+        ion_chan_def_file="",
+    ):
+        """Add channel density.
+
+        :param nml_cell_doc: cell NeuroML document to which channel density is to be added
+        :type nml_cell_doc: NeuroMLDocument
+        :param cd_id: id for channel density
+        :type cd_id: str
+        :param ion_channel: name of ion channel
+        :type ion_channel: str
+        :param cond_density: value of conductance density with units
+        :type cond_density: str
+        :param erev: value of reversal potential with units
+        :type erev: str
+        :param group: segment groups to add to
+        :type group: str
+        :param ion: name of ion
+        :type ion: str
+        :param ion_chan_def_file: path to NeuroML2 file defining the ion channel, if empty, it assumes the channel is defined in the same file
+        :type ion_chan_def_file: str
+        """
+        cd = self.biophysical_properties.membrane_properties.add(
+            "ChannelDensity",
+            id=cd_id,
+            segment_groups=group,
+            ion=ion,
+            ion_channel=ion_channel,
+            erev=erev,
+            cond_density=cond_density,
+        )
+
+        if len(ion_chan_def_file) > 0:
+            if (
+                self.component_factory("IncludeType", ion_chan_def_file)
+                not in nml_cell_doc.includes
+            ):
+                nml_cell_doc.add("IncludeType", ion_chan_def_file)
+
+    def setup_nml_cell(self, use_convention=True):
+        """Correctly initialise a NeuroML cell.
+
+        To be called after a new component has been created. Note that
+        `utils.component_factory` calls this for users when creating a new Cell
+        component.
+
+        Initialises the cell with these properties assigning IDs where applicable:
+
+        - Morphology: "morphology"
+        - BiophysicalProperties: "biophys"
+        - MembraneProperties
+        - IntracellularProperties
+
+        if `use_convention` is True, it also creates some default SegmentGroups for
+        convenience:
+
+        - "all", "soma_group", "dendrite_group", "axon_group" which
+          are used by other helper functions to include all, soma, dendrite, and
+          axon segments respectively.
+
+        Note that since this cell does not currently include a segment in its
+        morphology, it is *not* a valid NeuroML construct. Use the `add_segment`
+        function to add segments. `add_segment` will also populate the default
+        segment groups this creates.
+
+        :param id: id of the cell
+        :type id: str
+        :param use_convention: whether helper segment groups should be created using the default convention
+        :type use_convention: bool
+        :returns: None
+        :rtype: None
+
+        """
+        # do not validate yet, because segments are required
+        self.add("Morphology", id="morphology", validate=False)
+
+        membrane_properties = self.component_factory("MembraneProperties")
+        intracellular_properties = self.component_factory("IntracellularProperties")
+
+        self.biophysical_properties = self.component_factory(
+            "BiophysicalProperties",
+            id="biophys",
+            intracellular_properties=intracellular_properties,
+            membrane_properties=membrane_properties,
+        )
+
+        if use_convention:
+            seg_group_all = self.component_factory("SegmentGroup", id="all")
+            seg_group_soma = self.component_factory(
+                "SegmentGroup",
+                id="soma_group",
+                neuro_lex_id=self.neuro_lex_ids["soma"],
+                notes="Default soma segment group for the cell",
+            )
+            seg_group_axon = self.component_factory(
+                "SegmentGroup",
+                id="axon_group",
+                neuro_lex_id=self.neuro_lex_ids["axon"],
+                notes="Default axon segment group for the cell",
+            )
+            seg_group_dend = self.component_factory(
+                "SegmentGroup",
+                id="dendrite_group",
+                neuro_lex_id=self.neuro_lex_ids["dend"],
+                notes="Default dendrite segment group for the cell",
+            )
+            # skip validation: segments etc needed, cell is invalid
+            self.morphology.add(seg_group_all, validate=False)
+            self.morphology.add(seg_group_soma, validate=False)
+            self.morphology.add(seg_group_axon, validate=False)
+            self.morphology.add(seg_group_dend, validate=False)
 
     # end class Cell
 
@@ -61902,7 +62249,8 @@ class IF_cond_exp(basePyNNIaFCondCell):
     :type e_rev_E: none
     :param e_rev_I: This parameter is never used in the NeuroML2 description of this cell! Any synapse producing a current can be placed on this cell
     :type e_rev_I: none
-    :param tau_refrac:
+    :p
+    aram tau_refrac:
     :type tau_refrac: none
     :param v_thresh:
     :type v_thresh: none
