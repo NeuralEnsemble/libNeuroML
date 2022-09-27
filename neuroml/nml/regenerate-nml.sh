99eb8c7822f8945c96ff363d6fa765dcb5813d3c
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-09-27 14:39:55 +0100

    feat(nml.py): update regenerate script to add annotations

--- neuroml/nml/regenerate-nml.sh
+++ neuroml/nml/regenerate-nml.sh
@@ -26,6 +26,17 @@ regenerate () {
         # correct path to generatedssupersuper module file
         sed -i 's/from generatedssupersuper/from .generatedssupersuper/' nml.py
         sed -i 's/from generatedscollector/from .generatedscollector/' nml.py
+
+        # replace default arguments (None) with some hint of what's expected
+        # must be run from top level because of all the imports we
+        # have---otherwise it errors
+        echo "Modifying default arguments using annotate_nml and sed"
+        pushd ../../ > /dev/null 2>&1
+            # generates the sed-script.txt file
+            python -m neuroml.nml.annotate_nml
+        popd > /dev/null 2&>1
+        # apply the sed transformations
+        sed -i -f ../../sed-script.txt nml.py
     else
         echo "GenerateDS not installed"
         echo "Run: pip install generateds"
