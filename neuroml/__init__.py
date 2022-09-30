676e479ec6bc886b6c71e2c6cc36ace3601b8c37
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-09-30 16:37:08 +0100

    feat(version): ensure `neuroml.__version__` is still usable
    prevent an API break

--- neuroml/__init__.py
+++ neuroml/__init__.py
@@ -1,2 +1,4 @@
 from .nml.nml import *  # allows importation of all neuroml classes
-from .__version__ import *
+from .__version__ import __version__ as __version__
+from .__version__ import __version_info__ as __version__info__
+from .__version__ import current_neuroml_version as current_neuroml_version
