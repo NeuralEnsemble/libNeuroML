66e922c5b8419f6e608bf9282ad3e022f8c4e768
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2022-10-11 13:26:23 +0100

    feat(Cell): add method to create segment groups of unbranched branches

--- neuroml/nml/helper_methods.py
+++ neuroml/nml/helper_methods.py
@@ -1969,6 +1969,112 @@ cell_methods = MethodSpec(
 
         self.reorder_segment_groups()
         return self.get_segment_group(group_id)
+
+    def create_unbranched_segment_group_branches(self, root_segment_id: int, use_convention=True):
+        """Organise the segments of the cell into new segment groups that each
+        form a single contiguous unbranched cell branch.
+
+        Note that the first segment (root segment) of a branch must have a proximal
+        point that connects it to the rest of the neuronal morphology. If, when
+        constructing these branches, a root segment is found that does not include
+        a proximal point, one will be added using the `get_actual_proximal` method.
+
+        No other changes will be made to any segments, or to any pre-existing
+        segment groups.
+
+        :param root_segment_id: id of segment considered the root of the tree,
+            generally the first soma segment
+        :type root_segment_id: int
+        :param use_convention: toggle using NeuroML convention for segment groups
+        :type use_convention: bool
+        :returns: modified cell with new section groups
+        :rtype: neuroml.Cell
+
+        """
+        # get morphology tree
+        morph_tree = self.get_segment_adjacency_list()
+
+        # initialise root segment and first segment group
+        seg = self.get_segment(root_segment_id)
+        group_name = f"seg_group_{len(self.morphology.segment_groups) - 1}_seg_{seg.id}"
+        new_seg_group = self.add_unbranched_segment_group(group_name)
+
+        # run recursive function
+        self.__sectionise(root_segment_id, new_seg_group, morph_tree)
+
+
+    def __sectionise(self, root_segment_id: int, seg_group:
+                     SegmentGroup, morph_tree: dict[int, list[int]]):
+        """Main recursive sectionising method.
+
+        :param root_segment_id: id of root of branch
+        :type root_segment_id: int
+        :returns: TODO
+
+        """
+        # print(f"Processing element: {root_segment_id}")
+
+        try:
+            children = morph_tree[root_segment_id]
+            # keep going while there's only one child
+            # no need to use recursion here---hits Python's recursion limits in
+            # long segment groups
+            # - if there are no children, it'll go to the except block
+            # - if there are more than one children, it'll go to the next
+            # conditional
+            while len(children) == 1:
+                seg_group.add("Member", segments=root_segment_id)
+                root_segment_id = children[0]
+                children = morph_tree[root_segment_id]
+            # if there are more than one children, we've reached the end of this
+            # segment group but not of the branch. New segment groups need to start
+            # from here.
+            if len(children) > 1:
+                # this becomes the last segment of the current segment group
+                seg_group.add("Member", segments=root_segment_id)
+
+                # each child will start a new segment group
+                for child in children:
+                    seg = self.get_segment(child)
+                    # Ensure that a proximal point is set.
+                    # This is required for the first segment of unbranched segment
+                    # groups
+                    seg.proximal = self.get_actual_proximal(seg.id)
+                    group_name = f"seg_group_{len(self.morphology.segment_groups) - 1}_seg_{seg.id}"
+                    new_seg_group = self.add_unbranched_segment_group(group_name)
+
+                    self.__sectionise(child, new_seg_group, morph_tree)
+        # if there are no children, it's a leaf node, so we just add to the current
+        # seg_group and do nothing else
+        except KeyError:
+            seg_group.add("Member", segments=root_segment_id)
+
+
+    def get_segment_adjacency_list(self) -> dict[int, list[int]]:
+        """Get the adjacency list of all segments in the cell morphology.
+        Returns a dict where each key is a parent segment, and the value is the
+        list of its children segments.
+
+        Segment without children (leaf segments) are not included as parents in the
+        adjacency list.
+
+        :returns: dict with parent segments as keys and their children as values
+        :rtype: dict
+
+        """
+        # create data structure holding list of children for each segment
+        child_lists = {}  # type: dict[int, list[int]]
+        for segment in self.morphology.segments:
+            try:
+                parent = segment.parent.segments
+                if parent not in child_lists:
+                    child_lists[parent] = []
+                child_lists[parent].append(segment.id)
+            except AttributeError:
+                print(f"Warning: Segment: {segment} has no parent")
+
+        return child_lists
+
     ''',
     class_names=("Cell"),
 )
